8d0b81845276ba156684846a960e94835ae4482e
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2023-01-24 05:36:50 +0200

    redesign book card

--- app/components/BookCard.tsx
+++ app/components/BookCard.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from '../styles/BookCard.module.css';
 import Link from 'next/link';
-
+import { BsBookFill } from 'react-icons/bs';
 type Props = {
   book_id: number;
   book_name: string;
@@ -14,24 +14,25 @@ type Props = {
 // Book Card component.
 const BookCard = (props: Props) => {
   return (
+    <>
     <Link
       href={`/bookcategories/${props.category_id}/${props.book_id}`}>
       <span  className="text-decoration-none">
         <div
           className={`d-flex align-items-center justify-content-center ${styles.cardContainer}`}>
-          <div className="d-flex flex-column text-center p-5">
-            <span className="fs-5 fw-semibold text-black">
-              {props.book_name}
-              {props.author_name}
-            </span>
-            <span className="fs-5 fw-semibold text-black">
-              {props.category_name}
-              {props.book_rate}
+          <div className="d-flex flex-column p-5 ">
+          <span className={` fs-4 fw-bold ${styles.textStyle} `}>
+            <BsBookFill size={60} className=" align-center d-block"/>
+            <span className={`${styles.textStyle2} `}> {props.book_name} </span><br/>
+              Author : <span className={`${styles.textStyle} `}>{props.author_name}</span> <br/>
+              Category : <span className={`${styles.textStyle} `}> {props.category_name} </span> <br/>
+               Rate : <span className={` ${styles.textStyle} `}>{props.book_rate}</span>
             </span>
           </div>
         </div>
       </span>
     </Link>
+  </>
   );
 };
 
