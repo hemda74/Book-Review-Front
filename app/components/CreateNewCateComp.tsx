1e7a46ccafb96d5d0d3aee9acd4b0ad55338eeef
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2023-01-29 04:14:53 +0200

    update create new category

--- app/components/CreateNewCateComp.tsx
+++ app/components/CreateNewCateComp.tsx
@@ -11,9 +11,6 @@ type Props = {
   };
 
 const CreateNewCategoryComp = function (props: Props) {
-    const [isPassError, setIsPassError] = useState(false);
-    const [isPassMatchError, setIsPassMatchError] = useState(false);
-    const [isOldPassError, setIsOldPassError] = useState(false);
     const [isSuccess, setIsSuccess] = useState(false);
     const [isPending, setIsPending] = useState(false);
     // declaring next router in a variable.
@@ -23,7 +20,7 @@ const CreateNewCategoryComp = function (props: Props) {
       };
     return (
         <>
-            <div className="d-flex align-items-center justify-content-center mb-5 min-vh-100">
+            <div className="d-flex align-items-center justify-content-center mb-5 vh-100">
                 <div className="card border-light ">
                     <div className={`card-body  ${styles.cardBodyContainer}`}>
                         {/* design heading section */}
@@ -31,7 +28,7 @@ const CreateNewCategoryComp = function (props: Props) {
                             <h3 className='m-0' data-trans="">New Category</h3>
                         </div>
                         <div className='all inputs conainer row '>
-                            <div className="firstinputs  col-12">
+                            <div className="col-12">
                                 <div className="form-group pt-4">
                                     <label className={`col-form-label mt-4${styles.colFormLabel}}`} htmlFor="category-name-input" data-trans=""><span className={`${styles.inputLabel}`}>Site Name*</span></label>
                                     <input type="text" className={`form-control ${styles.inputWidth}`}  id="site-name-input" />
---
+++ app/pages/books/create_new.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const create_new = () => {
+  return (
+    <div>create_new</div>
+  )
+}
+
+export default create_new
