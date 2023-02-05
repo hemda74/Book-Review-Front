684cc2497e402a98990a0dcb40a8ae34f76f916b
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2023-02-06 01:39:25 +0200

    update book card sizes

--- app/components/BookCard.tsx
+++ app/components/BookCard.tsx
@@ -16,14 +16,14 @@ const BookCard = (props: Props) => {
     <>
     <Link
       href={`/books/${props.book_id}`}>
-        <div className={`card ${styles.cardContainer}`}>
+        <div className={`card pt-5 ${styles.cardContainer}`}>
           {/* here we can can add book image insted of icon */}
               <BsBookFill  className={`${styles.cardImage}`}/>
               <div className="text-center">
-                    <span className={`fs-2 d-block  ${styles.textTitle} ${styles.textStyle}`}>
+                    <span className={`fs-2 d-block mt-3 ${styles.textTitle} ${styles.textStyle}`}>
                       {props.book_name}
                     </span>
-                    <div className="mt-2 d-flex row">
+                    <div className="mt-4 d-flex row">
                           <div className="col-1">
                           </div>
                           <span className={`fs-6 col-5 p-2 myInfo rounded me-2`}>
@@ -34,7 +34,7 @@ const BookCard = (props: Props) => {
                           </span>
                     </div>
               </div>
-              <span className={`fs-4 mt-3 ms-3 text-center ${styles.rateSpan}`}>
+              <span className={`fs-4 mt-4 ms-3 text-center fw-semibold ${styles.rateSpan}`}>
                     Rate: {props.book_rate}
                     <span className={`${styles.iconStyle}`}> <BsStarFill size={20} className=' mb-2 mt-0'/>
                     </span>
