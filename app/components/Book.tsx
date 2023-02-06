9bf36b3728ba1d7e0d8936b56c67e998ae6b3b06
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2023-02-06 02:22:19 +0200

    update book component

--- app/components/Book.tsx
+++ app/components/Book.tsx
@@ -24,51 +24,66 @@ const Book = (props:Props) => {
       router.push('/books');
     };
   return (
-    <>
-<div className={` row ${styles.bookCont}`} >
-    <div className="col-lg-6 ">
-        <Image src={bookimage} className={` ${styles.cardImage}`} alt=""/>
-    </div>
-    <div className="col-lg-4 text-center m-auto  ">
-        <span className={` fw-bold  p-3  rounded fs-1`}>
-            {props.book.book_name} 
-        </span>
-  <p className="card-text-details mt-3 mt-4 fs-3 ">
-                                Written By : {props.book.author_name}
-                            </p>
-                            <p className="card-text-details  mt-3 fs-3 ">
-                                Category :{props.book.category_name} 
-                            </p>
-                            <p className="card-text-details  fs-3 mt-3  mb-5">
-                                Rate :  {props.book.book_rate} 
-                                <span className={`${styles.iconStyle} `}> 
-                                <BsStarFill className='mb-2'/></span>
-                            </p>
- 
-</div>
-<div className={`d-flex justify-content-center mt-5 ${styles.buttonsStyles}`}>
-                        <button
-                            type="button"
-                            className={`btn myPrimary-outline ${styles.confirmBtn} me-5 ms-2`}
-                            onClick={() => handleCancelBtn()}
-                            disabled={isPending}>
-                            <span className="fs-5 fw-semibold p-4 " data-trans="cancel">
-                            Cancel
-                            </span>
-                            </button>
-                            <button
-                            type="button"
-                            className={`btn myPrimary-outline ${styles.confirmBtn} me-5 ms-2`}
-                            onClick={() => handleCancelBtn()}
-                            disabled={isPending}>
-                            <span className="fs-5 fw-semibold p-4 " data-trans="cancel">
-                            Cancel
-                            </span>
-                            </button>
-                         
-                    </div>
-</div>    
-  </>
+ <>
+    <div className={` row ${styles.bookCont}`} >
+        <div className="col-3">
+        </div>
+        <div className="col-lg-4">
+            <Image src={bookimage} className={` ${styles.cardImage} ms-2  `} alt=""/>
+        </div>
+        <div className={`col-lg-2 text-center m-auto`}>
+            <span className={` fw-bold  mt-5  fs-1`}>
+                {props.book.book_name} 
+            </span>
+            <p className="card-text-details mt-3 mt-4 fs-3 ">
+                Written By : {props.book.author_name}
+            </p>
+            <p className="card-text-details  mt-3 fs-3 ">
+                Category :{props.book.category_name} 
+            </p>
+            <p className="card-text-details  fs-3 mt-3  mb-5">
+                Rate :  {props.book.book_rate} 
+                <span className={`${styles.iconStyle}`}> 
+                <BsStarFill className='mb-2'/></span>
+            </p>
+            <button
+                type="button"
+                className={`btn myPrimary ${styles.confirmBtn}`}
+                disabled={isPending}>
+                {!isPending && (
+                <span className="fs-5 fw-semibold" data-trans="confirm">
+                    Add Review 
+                </span>
+                )}
+                {isPending && <span>Loading...</span>}
+            </button>
+        </div>
+        <div className="col-3">
+        </div>
+      <div className={`d-flex justify-content-center mt-5 ${styles.buttonsStyles}`}>
+            <button
+                type="button"
+                className={`btn myDanger ${styles.confirmBtn} me-5 ms-2`}
+                onClick={() => handleCancelBtn()}
+                disabled={isPending}>
+                <span className="fs-5 fw-semibold" data-trans="cancel">
+                Delete Book
+                </span>
+                </button>
+                <button
+                type="button"
+                className={`btn myInfo ${styles.confirmBtn}`}
+                disabled={isPending}>
+                {!isPending && (
+                <span className="fs-5 fw-semibold" data-trans="confirm">
+                    Update Data
+                </span>
+                )}
+                {isPending && <span>Loading...</span>}
+            </button>
+          </div>
+    </div>    
+</>
   )
 }
-export default Book;
+export default Book ;
